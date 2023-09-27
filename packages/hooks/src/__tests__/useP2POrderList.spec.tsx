64d94e4769668206ad57c88f32743cdad745da81
yaswanth-deriv <121096908+yaswanth-deriv@users.noreply.github.com>
2023-09-27 16:07:26 +0400

    refactor: to add p2p order list hook

---
+++ packages/hooks/src/__tests__/useP2POrderList.spec.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { APIProvider, usePaginatedFetch } from '@deriv/api';
+import { StoreProvider, mockStore } from '@deriv/stores';
+import { renderHook } from '@testing-library/react-hooks';
+import useP2POrderList from '../useP2POrderList';
+
+type TWrapper = {
+    children: JSX.Element;
+};
+
+jest.mock('@deriv/api', () => ({
+    ...jest.requireActual('@deriv/api'),
+    usePaginatedFetch: jest.fn(),
+}));
+
+const mockUsePaginatedFetch = usePaginatedFetch as jest.MockedFunction<typeof usePaginatedFetch<'p2p_order_list'>>;
+
+describe('useP2POrdertList', () => {
+    it('should return undefined if there is no response', () => {
+        const mock_store = mockStore({});
+        // @ts-expect-error need to come up with a way to mock the return type of usePaginatedFetch
+        mockUsePaginatedFetch.mockReturnValue({});
+
+        const wrapper = ({ children }: TWrapper) => (
+            <APIProvider>
+                <StoreProvider store={mock_store}>{children}</StoreProvider>
+            </APIProvider>
+        );
+
+        const { result } = renderHook(() => useP2POrderList({}), { wrapper });
+        expect(result.current.data).toBe(undefined);
+    });
+    it('should return the p2p_advert_list object from response', () => {
+        const mock_store = mockStore({
+            client: { currency: 'USD' },
+        });
+
+        mockUsePaginatedFetch.mockReturnValue({
+            data: {
+                p2p_order_list: {
+                    list: [
+                        // @ts-expect-error need to come up with a way to mock the return type of usePaginatedFetch
+                        {
+                            id: '1',
+                            account_currency: 'USD',
+                            amount: 1000,
+                            contact_info: 'Created by script. Please call me 02203400',
+                        },
+                    ],
+                },
+            },
+        });
+
+        const wrapper = ({ children }: TWrapper) => (
+            <APIProvider>
+                <StoreProvider store={mock_store}>{children}</StoreProvider>
+            </APIProvider>
+        );
+
+        const { result } = renderHook(() => useP2POrderList({}), { wrapper });
+        const p2p_order_list = result.current.data;
+        expect(p2p_order_list).toHaveLength(1);
+        expect(p2p_order_list?.[0].amount).toBe(1000);
+        expect(p2p_order_list?.[0].contact_info).toBe('Created by script. Please call me 02203400');
+    });
+});
---
+++ packages/hooks/src/useP2POrderList.ts
@@ -0,0 +1,48 @@
+import { usePaginatedFetch, useSubscription } from '@deriv/api';
+import { useEffect, useState } from 'react';
+
+type TOrder = NonNullable<
+    NonNullable<ReturnType<typeof useSubscription<'p2p_order_list'>>['data']>['p2p_order_list']
+>['list'][number];
+
+/** This custom hook returns the list of active orders by calling 'p2p_order_list' endpoint */
+const useP2POrderList = (
+    payload: NonNullable<Parameters<typeof usePaginatedFetch<'p2p_order_list'>>[1]>['payload']
+) => {
+    const { data, ...rest } = usePaginatedFetch('p2p_order_list', { payload });
+    const { data: subscription_data, subscribe, ...subscription_rest } = useSubscription('p2p_order_list');
+    const [newOrders, setNewOrders] = useState<TOrder[]>();
+
+    useEffect(() => {
+        subscribe();
+    }, [subscribe]);
+
+    useEffect(() => {
+        setNewOrders(old_orderList => {
+            const new_orderList = subscription_data?.p2p_order_list?.list as TOrder[] | undefined;
+
+            if (!new_orderList) return old_orderList;
+            if (!old_orderList) return new_orderList;
+            const updated_orderList = [...old_orderList];
+            new_orderList.forEach(new_order => {
+                const index = updated_orderList.findIndex(old_order => old_order.id === new_order.id);
+
+                if (index === -1) {
+                    updated_orderList.push(new_order);
+                } else {
+                    updated_orderList[index] = new_order;
+                }
+            });
+
+            return updated_orderList;
+        });
+    }, [data?.p2p_order_list?.list]);
+
+    return {
+        data: newOrders,
+        ...rest,
+        ...subscription_rest,
+    };
+};
+
+export default useP2POrderList;
